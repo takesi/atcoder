26e5a85a39bb72803c844729d1ffe2f69fa20cef
takesi <takeshi.ito@JHQADM001632ML.local>
2020-02-23 09:06:02 +0900

    mint update

--- lib/mint.cpp
+++ lib/mint.cpp
@@ -19,9 +19,7 @@ const ll LINF = 1001002003004005006ll;
 
 struct mint {
   ll v;
-  mint() : v(0) {}
-  mint(ll v) : v((v % MOD + MOD) % MOD) {}
-  operator ll() const { return v; }
+  mint(ll v = 0) : v((v % MOD + MOD) % MOD) {}
   mint pow(ll t) const;
   mint inv() const;
 };
@@ -47,44 +45,62 @@ bool operator!=(mint a, mint b) { return a.v != b.v; }
 istream& operator>>(istream& os, mint& a) { return (os >> a.v); }
 ostream& operator<<(ostream& os, const mint& a) { return (os << a.v); }
 
-mint mfact(int n) { // n! (mod MOD)
-  static vector<mint> fact(1, 1);
+// こちらに置き換えると若干高速になる
+// mint& operator+=(mint& a, mint b) {
+//   if ((a.v += b.v) >= MOD) a.v -= MOD;
+//   return a;
+// }
+// mint& operator-=(mint& a, mint b) {
+//   if ((a.v += MOD - b.v) >= MOD) a.v -= MOD;
+//   return a;
+// }
+// mint& operator*=(mint& a, mint b) {
+//   (a.v *= b.v) %= MOD;
+//   return a;
+// }
+
+mint mfact(int n, bool inv = false) {
+  static vector<mint> fact(1, 1), ifact(1, 1);
   int pn = fact.size();
   if (pn < n + 1) {
     fact.resize(n + 1);
+    ifact.resize(n + 1);
     for (int i = pn; i <= n; i++) fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= pn; --i) ifact[i - 1] = ifact[i] * i;
   }
-  return fact[n];
+  return inv ? ifact[n] : fact[n];
 }
 mint nPk(int n, int k) {
   if (n < 0 || k < 0 || n < k) return 0;
   if (k == 0) return 1;
-  return mfact(n) * mfact(n - k).inv();
+  return mfact(n) * mfact(n - k, true);
 }
 mint nCk(int n, int k) {
   if (n < 0 || k < 0 || n < k) return 0;
   if (k == 0) return 1;
-  return mfact(n) * mfact(k).inv() * mfact(n - k).inv();
+  return mfact(n) * mfact(k, true) * mfact(n - k, true);
 }
 
-// 何度もcomb/permを呼ぶ場合はこっちの方が早いかも
+// 何度もnPk/nCkを呼ぶ場合はこっちの方が早いかも
 struct mcount {
   vector<mint> fact;  // n! (mod MOD)
-  vector<mint> ifact; // k!^{M-2} (mod MOD)
+  vector<mint> ifact; // n!^{M-2} (mod MOD)
   mcount(int n) : fact(n + 1), ifact(n + 1) {
     fact[0] = 1;
     for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
     ifact[n] = fact[n].inv();
     for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
   }
-  mint perm(int n, int k) { // nPk (mod MOD)
+  mint nPk(int n, int k) { // nPk (mod MOD)
     if (k < 0 || n < k) return 0;
     return fact[n] * ifact[n - k];
   }
-  mint comb(int n, int k) { // nCk (mod MOD)
+  mint nCk(int n, int k) { // nCk (mod MOD)
     if (k < 0 || n < k) return 0;
     return fact[n] * ifact[k] * ifact[n - k];
   }
+  mint operator()(int n, int k) { return nCk(n, k); }
 };
 
 int main() {
@@ -98,13 +114,17 @@ int main() {
   c = rand();
   d = rand();
   dump(a, b, c, d);
+  a = a + 1;
+  dump(a, b, c, d);
   cout << (a * b + c - d) << endl;
 
-  mcount ms(10000);
   int n = rand() % 10000;
   int k = rand() % n;
-  cout << "nPk : " << ms.perm(n, k) << endl;
-  cout << "nCk : " << ms.comb(n, k) << endl;
+
+  mcount ms(10000);
+  cout << "nPk : " << ms.nPk(n, k) << endl;
+  cout << "nCk : " << ms.nCk(n, k) << endl;
+  cout << "nCk : " << ms(n, k) << endl;
 
   cout << "nPk : " << nPk(n, k) << endl;
   cout << "nCk : " << nCk(n, k) << endl;
